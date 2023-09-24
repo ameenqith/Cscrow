d742195afdcdb8a358eecc4bc797d7c45f04f56f
Nahid Guliyev <quliyevn633@gmail.com>
2023-09-24 13:50:53 +0200

    constant rewardPool path

--- Context/constants.js
+++ Context/constants.js
@@ -1,4 +1,6 @@
 import escrow from "./EscrowABI.json";
+import rewardPool from "./RewardPoolABI.json";
+
 export const escrowAddress = "0x80ab86e995b35594ceb6a15b78909ec450e5f190";
 
 // "0xB23A6A3a053C2bfab042b0Adc2c27b6384C31279";
